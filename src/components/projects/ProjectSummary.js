a876c7ce607d4f21a553b6b9c9c19b622d84bcbd
arenthus <krzysiek177@gmail.com>
2020-04-25 23:52:04 +0200

    adding fetching data from redux into component

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import ProjectList from './ProjectList'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
 
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                 <p>Posted by the Arenthus</p>
                 <p className="grey-text">3rd September, 2am</p>
             </div>
