9f196ed9b4001caeab73c9f5c1b04a9302cefcf1
arenthus <krzysiek177@gmail.com>
2020-04-26 00:52:01 +0200

    added firestorm and firebase tools for react

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestorm }) => {
         // make async call to database
         dispatch({ type: 'CREATE_PROJECT', project});
     }
