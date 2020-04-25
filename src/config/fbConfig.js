deaa850c4de2633f6fddfea19d421b3548f55877
arenthus <krzysiek177@gmail.com>
2020-04-26 00:33:33 +0200

    created firebase project and added it into app

---
+++ src/config/fbConfig.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDH1K_C34ofnt5VMq1MA-RqNi0YwmGCYNg",
+    authDomain: "react-redux-projectmanagment.firebaseapp.com",
+    databaseURL: "https://react-redux-projectmanagment.firebaseio.com",
+    projectId: "react-redux-projectmanagment",
+    storageBucket: "react-redux-projectmanagment.appspot.com",
+    messagingSenderId: "690983578813",
+    appId: "1:690983578813:web:853e72ceff9ce9fd2cdbd4",
+    measurementId: "G-D5EW9T65VL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  firebase.firestore().settings({ timestampsInSnapshots: true});
+
+
+  export default firebase;
